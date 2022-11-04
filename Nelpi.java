f63f18e2639669a1a5360aeed43d0d9085ceac4c
Juni Saragih <nelpisaragih2306@gmail.com>
2022-11-04 17:08:44 +0700

    contoh_python2

---
+++ Nelpi.java
@@ -0,0 +1,25 @@
+/*
+Nama : Nelpi Juniati Saragih
+Kelas : 1IA28
+NPM : 50422027
+*/
+public class Nelpi {
+    public static void main(String [] args){
+        System.out.println("Tugas Latihan Variable Increment/Decrement");
+        System.out.println();
+
+        int a = 23;
+        System.out.println("-Pre Increment-");
+        System.out.println("Variable a      : "+ a);
+        System.out.println("Variable a      : "+ ++a);
+        System.out.println("Jadi Variable a : "+ a);
+
+        System.out.println();
+
+        int b = 27;
+        System.out.println("-Post Increment-");
+        System.out.println("Variable b      : "+ b);
+        System.out.println("Variable b      : "+ b++);
+        System.out.println("Jadi Variable b : "+ b);
+    }
+}
